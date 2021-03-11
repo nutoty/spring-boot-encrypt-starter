42d9be77374d678b99757ceefe7cad0e6c94b981
nutoty <971189868@qq.com>
2021-03-11 12:59:13 +0800

    first commit

---
+++ src/main/java/org/javaboy/springbootencryptstarter/SpringBootEncryptStarterApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.springbootencryptstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEncryptStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEncryptStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaboy/springbootencryptstarter/SpringBootEncryptStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.springbootencryptstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEncryptStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
